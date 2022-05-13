ba6c94f27d53e20cf31e010a2ebdfa98e58321a8
Greg Parr <greg.parr@blueyonder.co.uk>
2022-05-13 20:56:16 +0100

    fixed the autocomplete problem with apostrophes

--- prototype/app.js
+++ prototype/app.js
@@ -26,7 +26,7 @@ function autocomplete(inp, arr) {
             //b.innerHTML += arr[i].substr(val.length);
             b.innerHTML = arr[i];
             /*insert a input field that will hold the current array item's value:*/
-            b.innerHTML += "<input type='hidden' value='" + arr[i].replace("'","") + "'>";
+            b.innerHTML += '<input type=\'hidden\' value="' + arr[i] + '">';
             /*execute a function when someone clicks on the item value (DIV element):*/
                 b.addEventListener("click", function(e) {
                 /*insert the value for the autocomplete text field:*/
