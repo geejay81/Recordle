1dbed9d716af819505a3a384617206cf92c26601
Greg Parr <greg.parr@blueyonder.co.uk>
2022-05-31 22:21:21 +0100

    adjusted tomorrows album data source

--- recordle/src/app/services/data.service.ts
+++ recordle/src/app/services/data.service.ts
@@ -13,6 +13,6 @@ export class DataService {
   ) { }
 
   getAlbumSourceData(): Observable<IAlbum[]> {
-    return this.http.get<IAlbum[]>('/assets/data/albums.json');
+    return this.http.get<IAlbum[]>('/assets/data/albums.json?v31-5-22');
   }
 }
