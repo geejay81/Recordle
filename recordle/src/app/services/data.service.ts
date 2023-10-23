ccb85841810281c3a43d7a4d623253c65159872d
Greg Parr <greg.parr@blueyonder.co.uk>
2023-10-23 22:42:49 +0100

    updated api to use CDN

--- recordle/src/app/services/data.service.ts
+++ recordle/src/app/services/data.service.ts
@@ -1,3 +1,4 @@
+import { environment } from './../../environments/environment';
 import { Observable } from 'rxjs';
 import { IApiResponse } from './../models/IAlbum';
 import { Injectable } from '@angular/core';
@@ -22,7 +23,6 @@ export class DataService {
   ) { }
 
   getAlbumApiData(): Observable<IApiResponse> {
-    return this.http.get<IApiResponse>(
-      "https://v6qz2c5y.api.sanity.io/v2021-10-21/data/query/production?query=*%5B_type%20%3D%3D%20%22album%22%5D%7B%0A%20%20'id'%3A%20gameId%2C%0A%20%20artist%2C%0A%20%20albumTitle%2C%0A%20%20embedKey%2C%0A%20%20'coverArt'%3A%20coverArt.asset-%3Eurl%0A%7D");
+    return this.http.get<IApiResponse>(environment.dataUrl);
   }
 }
