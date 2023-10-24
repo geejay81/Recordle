05bfb3a03e4e921cdcda6cacc732138df59ea33c
Greg Parr <greg.parr@blueyonder.co.uk>
2023-10-24 22:54:00 +0100

    fixed link to footer sass file from ts file

--- recordle/src/app/components/ko-fi/ko-fi.component.ts
+++ recordle/src/app/components/ko-fi/ko-fi.component.ts
@@ -4,7 +4,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ko-fi',
   templateUrl: './ko-fi.component.html',
-  styleUrls: ['./ko-fi.component.css']
+  styleUrls: []
 })
 export class KoFiComponent implements OnInit {
   ngOnInit() {
